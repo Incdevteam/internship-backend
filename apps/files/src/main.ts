ddef90760d32146b824f3626acae4c4cd8969956
HellCounT <a_killaheadz@live.ru>
2025-01-30 13:22:18 +0300

    fix: changed hostname to microservice name

--- apps/files/src/main.ts
+++ apps/files/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     {
       transport: Transport.TCP,
       options: {
-        host: 'platform-files-service-service',
+        host: '0.0.0.0',
         port: 3726,
       },
     },
